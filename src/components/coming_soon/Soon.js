79ec102565adcc5e613b42eb50a4841cec4ea25f
Petermuturi <pmuturi65@gmail.com>
2017-01-26 13:55:16 +0300

    top -section background

--- src/components/coming_soon/Soon.js
+++ src/components/coming_soon/Soon.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Container } from 'semantic-ui-react'
+
+import './soon.css'
 
 export default class Soon extends Component {
 	render() {
 		return (
-			<div>
-				
-			</div>
+			<Container className="home">
+				<div className="overlay">
+					<div className="pattern">
+
+					</div>
+				</div>
+			</Container>
 		);
 	}
 }
--- src/components/main/App.js
+++ src/components/main/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Soon from '../coming_soon/Soon'
 
 class App extends Component {
