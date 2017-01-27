c6e62d0b44c1362a6d09f07e888fa5076d1520fe
Petermuturi <pmuturi65@gmail.com>
2017-01-27 12:18:57 +0300

    changed from semantic ui to bootstrap

--- src/components/coming_soon/Soon.js
+++ src/components/coming_soon/Soon.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react'
-import { Container } from 'semantic-ui-react'
+import { Grid, Row, Col } from 'react-bootstrap'
 
 import './soon.css'
 
 export default class Soon extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			divider: {
+					"marginTop": 20,
+					"marginLeft": 35,
+					"width":2,
+					"height": 400,
+					"backgroundColor": "white"
+			} 
+		}
+	}
+
 	render() {
 		return (
-			<Container className="home">
+			<div className="home">
 				<div className="overlay">
 					<div className="pattern">
+						<Grid>
+							<Row>
+								<Col xsHidden md={4}>
+									<div style={this.state.divider}/>
+								</Col>
+      					<Col xs={12} md={8}>
 
+      					</Col>
+							</Row>
+						</Grid>
+
+			
 					</div>
 				</div>
-			</Container>
+			</div>
 		);
 	}
 }
